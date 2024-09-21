051cc4ee330c085339ab32761c6262af8a1a617d
kaytopod <kaytopod0119@gmail.com>
2024-09-21 20:31:40 +0900

    上下に移動

--- EnemyClass.py
+++ EnemyClass.py
@@ -6,29 +6,36 @@ class Enemy:
         self.image = pg.image.load(image_path)
         self.image = pg.transform.scale(self.image, (50, 50))
         self.rects = []  # 敵を管理するリスト
-        self.speeds = []  # 各敵のx方向の速度を管理するリスト
-        self.spawn_delay = 15  # 敵を出現させるフレーム数の間隔
+        self.speeds_x = []  # 各敵のx方向の速度を管理するリスト
+        self.speeds_y = []  #各敵のｙ方向の速度を管理するリスト
+        self.spawn_delay = 10  # 敵を出現させるフレーム数の間隔
         self.frame_count = 0  # フレームカウントの初期値
-        self.speed = 5  # 左右に動くスピード
+        self.speed_x = 5  # 左右に動くスピード
+        self.speed_y = 2 #上下に動くスピード
 
     def update(self, player_rect):
         self.frame_count += 1
         # 一定のフレーム数ごとに敵を生成
         if self.frame_count % self.spawn_delay == 0:
             new_enemy = pg.Rect(random.randint(0, 750), -50, 50, 50)
-            new_speed = self.speed  # 新しい敵の初期速度
+            new_speed_x = self.speed_x  # 新しい敵の初期速度
+            new_speed_y = self.speed_y
             self.rects.append(new_enemy)
-            self.speeds.append(new_speed)
+            self.speeds_x.append(new_speed_x)
+            self.speeds_y.append(new_speed_y)
 
         # 既存の敵の移動（左右と縦方向）
         for i in range(len(self.rects) - 1, -1, -1):  # リストを逆順にループ
             rect = self.rects[i]
-            rect.x += self.speeds[i]  # 敵のx座標に速度を適用
-            rect.y += 5  # 下方向への移動
+            rect.x += self.speeds_x[i]  # 左右方向への移動
+            rect.y += self.speeds_y[i]  # 上下下方向への移動
 
             # 画面端に来たら方向を反転
             if rect.x <= 0 or rect.x >= 750:
-                self.speeds[i] *= -1  # x方向の移動を反転
+                self.speeds_x[i] *= -1  # x方向の移動を反転
+            #画面上下端に来たら上下方向の速度を反転
+            if rect.y <= 0 or rect.y >= 550:
+                self.speeds_y[i] *= -1
 
             # プレイヤーとの衝突
             if rect.colliderect(player_rect):
@@ -37,7 +44,8 @@ class Enemy:
             # 画面外に出たら削除
             if rect.y > 600:
                 del self.rects[i]
-                del self.speeds[i]
+                del self.speeds_x[i]
+                del self.speeds_y[i]
 
         return False  # プレイヤーと衝突しなければFalseを返す
 
